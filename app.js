28ac0fa35a1e85b9346d44db25085d2774d50214
Pablo Jacobo <pablojacobo1998@gmail.com>
2018-10-08 09:47:21 -0600

    Deployment Config

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 
-let app = express();
+//Routes
+let routeUser = require('./userRoute');
 
-let PORT = 123;
+//INIT
+var app = express();
+
+//CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","POST, GET, PUT, DELETE, OPTIONS"); 
+    next();
+});
+
+
+let PORT = 3000;
+
 
 //IP 46.249.204.26:26109/api/v1/parents/1
+
+app.use('/usex/get', routeUser);
 
 app.listen(PORT, ()=>{
     console.log('BackEnd Online');
